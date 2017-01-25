6997b609de98f0ae9830eac3289bb6a3aae12e2a
pvegliante@fvi.edu <pvegliante@fvi.edu>
2017-01-24 21:37:18 -0500

    update to boot-strap

--- main.js
+++ main.js
@@ -1,37 +1,45 @@
 // http://fvi-grad.com:4004/email
 
 var x = document.querySelectorAll('li a');
-var redButton = document.getElementById('BigRedButton');
-redButton.addEventListener('click', function() {
-  console.log(x[0]);
-  x[0].classList.add('yellow-button');
-  x[1].classList.add('yellow-button');
-  x[2].classList.add('yellow-button');
-  x[3].classList.add('yellow-button');
-});
+// // var redButton = document.getElementById('BigRedButton');
+// // redButton.addEventListener('click', function() {
+//   console.log(x[0]);
+//   x[0].classList.add('yellow-button');
+//   x[1].classList.add('yellow-button');
+//   x[2].classList.add('yellow-button');
+//   x[3].classList.add('yellow-button');
+// });
 
 var form = $('#form');
 var sendBtn = $('#send-btn');
 var $spin = $('#dice');
+var audio = document.getElementById("audio");
 
+function play() {
+    audio.play();
+}
 
-setInterval(function(spin){
-  $spin.attr('src', './images/Dodecahedron3.gif');
+$('.btnSound').click(function() {
+    play();
+});
+
+setInterval(function(spin) {
+    $spin.attr('src', './images/Dodecahedron3.gif');
 }, 1800);
 
 
 form.submit(function(event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  $.ajax({
-    url: 'http://fvi-grad.com:4004/email',
-    method: 'POST',
-    data: form.serialize()
-  })
-  .done(function() {
-    sendBtn.val('Message Sent');
-  })
-  .fail(function() {
-    sendBtn.val('Message Failed');
-  })
+    $.ajax({
+            url: 'http://fvi-grad.com:4004/email',
+            method: 'POST',
+            data: form.serialize()
+        })
+        .done(function() {
+            sendBtn.val('Message Sent');
+        })
+        .fail(function() {
+            sendBtn.val('Message Failed');
+        })
 });
